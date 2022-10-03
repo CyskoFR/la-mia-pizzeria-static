70cb15404e3904477cb8deda8cd5200ea55789f7
CyskoFR <franc.sko91@gmail.com>
2022-10-03 16:16:53 +0200

    add class Pizza + index.cshtml table

---
+++ Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public double Price { get; set; }
+
+        public Pizza(string name, string description, string picture, double price)
+        {
+            Name = name;
+            Description = description;
+            Picture = picture;
+            Price = price;
+        }
+    }
+}
